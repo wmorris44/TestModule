609d639718c6a50cef1b4bec9d38fe52aae12701
Will Morris <wmorris@athenahealth.com>
2021-07-15 14:08:43 -0600

    commit

--- Package.swift
+++ Package.swift
@@ -23,7 +23,13 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "TestModule",
-            dependencies: []),
+            dependencies: [],
+            path: "./Sources",
+            resources: [.process("./Sources")],
+            swiftSettings: [
+              .define("SPM")
+            ]
+        ),
         .testTarget(
             name: "TestModuleTests",
             dependencies: ["TestModule"]),
---
+++ Sources/Bundle+Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Bundle+Extensions.swift
+//  
+//
+//  Created by Will Morris on 7/15/21.
+//
+
+import Foundation
+
+//#if !SPM
+//extension Bundle {
+//  static var module: Bundle { Bundle(identifier: "com.library.example")! }
+//}
+//#endif
